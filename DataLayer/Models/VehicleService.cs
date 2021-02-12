d5041848e5874c94e814fcdbc35749fce8dcb50a
Mike <michael-wilkins@sky.com>
2021-02-12 21:35:08 +0000

    First commit

---
+++ DataLayer/Models/VehicleService.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Models
+{
+    public class VehicleService
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        public string Website { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
