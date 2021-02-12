e7e4d2018a1306118becd14d028299013ea138ff
Mike <michael-wilkins@sky.com>
2021-02-12 21:42:53 +0000

    Added DataLayer ApplicationDbContext service

---
+++ DataLayer/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.Services
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<VehicleService> VehicleServices { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ DataLayer/Services/IApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<VehicleService> VehicleServices { get; set; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        EntityEntry Remove([NotNullAttribute] object entity);
+        EntityEntry<TEntity> Remove<TEntity>([NotNullAttribute] TEntity entity) where TEntity : class;
+    }
+}
