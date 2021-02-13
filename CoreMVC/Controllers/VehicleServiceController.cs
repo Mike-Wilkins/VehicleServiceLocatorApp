e2d5c85138d8cff8f92c7524874c383ba2ee2f53
Mike <michael-wilkins@sky.com>
2021-02-13 13:14:10 +0000

    Added Create new service

---
+++ CoreMVC/Controllers/VehicleServiceController.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer.Models;
+using DataLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Controllers
+{
+    public class VehicleServiceController : Controller
+    {
+        private IVehicleServiceRepository _db;
+
+        public VehicleServiceController(IVehicleServiceRepository db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var vehicleServiceList = await _db.GetAllServices();
+            return View(vehicleServiceList);
+        }
+
+        //GET: VehicleService/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //POST: VehicleService/Create
+        public async Task<IActionResult> Create(VehicleService vehicleService)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _db.Add(vehicleService);
+            var vehicleServiceList = await _db.GetAllServices();
+            return View("Index", vehicleServiceList);
+        }
+
+    }
+}
