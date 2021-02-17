ac31a9352d1837cd3fc8b2ae8ea6ca69a684cae3
Mike <michael-wilkins@sky.com>
2021-02-17 14:12:34 +0000

    Custom icons added to Layout

--- CoreMVC/Startup.cs
+++ CoreMVC/Startup.cs
@@ -2,15 +2,10 @@ using DataLayer.Repositories;
 using DataLayer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreMVC
 {
@@ -57,7 +52,7 @@ namespace CoreMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=VehicleService}/{action=Index}/{id?}");
             });
         }
     }
