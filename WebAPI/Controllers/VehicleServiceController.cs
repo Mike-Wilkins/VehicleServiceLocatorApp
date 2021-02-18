39b77f9b039e626544850bb9f316b9c55abaf26a
Mike <michael-wilkins@sky.com>
2021-02-18 12:23:19 +0000

    WebAPI created

---
+++ WebAPI/Controllers/VehicleServiceController.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Models;
+using DataLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleServiceController : ControllerBase
+    {
+        private IApplicationDbContext _context;
+        public VehicleServiceController(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<VehicleService>> GetAllVehicleServices()
+        {
+            var vehicleServiceList = _context.VehicleServices.OrderBy(m => m.Id).ToList();
+            return vehicleServiceList;
+        }
+    }
+}
